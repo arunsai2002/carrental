1e1fd1217208be7f5438d1e383fa6a2790bbfc9f
arunsai2002 <92771726+arunsai2002@users.noreply.github.com>
2023-09-26 16:56:10 +0530

    Add files via upload

---
+++ sourcecode/admin/includes/leftbar.php
@@ -0,0 +1,19 @@
+<nav class="ts-sidebar">
+	<ul>
+
+		<li><a href="#"><i class="fa fa-car"></i> Vehicles</a>
+			<ul>
+				<li><a href="post-avehical.php">Post a Vehicle</a></li>
+				<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+			</ul>
+		</li>
+
+		<li><a href="manage-bookings.php"><i class="fa fa-sitemap"></i> Bookings</a></li>
+		
+
+
+
+
+
+	</ul>
+</nav>
