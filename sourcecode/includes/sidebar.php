5c10681a2a1f09dd54cd5e5658ee7100fffd65ad
arunsai2002 <92771726+arunsai2002@users.noreply.github.com>
2023-09-26 17:01:47 +0530

    Add files via upload

---
+++ sourcecode/includes/sidebar.php
@@ -0,0 +1,10 @@
+<div class="profile_nav">
+          <ul>
+           
+            
+            <li><a href="vehical-details.php">My Booking</a></li>
+            <li><a href="registration.php">Sign In</a></li>
+            <li><a href="logout.php">Sign Out</a></li>
+          </ul>
+        </div>
+      </div>
